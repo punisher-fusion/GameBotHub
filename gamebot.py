dd3d059a411b53ac61bba6499ed6cc89f92f716f
Punisher Fusion <pnsgraal@gmail.com>
2018-10-22 17:51:00 -0200

    Update gamebot.py

--- gamebot.py
+++ gamebot.py
@@ -737,7 +737,7 @@ async def on_message(message):
             userid = message.content[a:]
             user = message.server.get_member(userid)
             await client.send_message(message.channel,
-                                      "**O usuario(a) <@{}> foi banido com sucesso do servidor.**".format(user.id))
+                                      "**O usuário(a) {} foi banido com sucesso do servidor.**".format(user.id))
             await client.ban(user, delete_message_days=1)
         except IndexError:
             await client.send_message(message.channel, "**Você deve especificar um usuario para banir!**")
